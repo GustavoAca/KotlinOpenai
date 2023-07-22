d7eac4f4ca953f6921a79a411070a5a86b72dd99
gustavo.acacio <gustavo.acacio@attornatus.com.br>
2023-07-21 21:21:28 -0300

    integrado

--- src/main/kotlin/com/group/Kotlin/service/impl/SimulateServiceImpl.kt
+++ src/main/kotlin/com/group/Kotlin/service/impl/SimulateServiceImpl.kt
@@ -14,7 +14,8 @@ class SimulateServiceImpl(
         private val teamService: TeamService,
         private val chatGptService: ChatGptService,
         @Value("\${openai.api-key}")
-        private val openAiApiKey: String,) : SimulateService {
+        private val openAiApiKey: String,
+) : SimulateService {
 
     val WOMEM_WORLD_CUP_2023_TEAMS = teamService.findAll();
 
@@ -22,22 +23,27 @@ class SimulateServiceImpl(
         val team1 = teamService.findById(timeDeCasa)
         val team2 = teamService.findById(timeDeFora)
 
-        val trainingData = WOMEM_WORLD_CUP_2023_TEAMS.joinToString( "\n" ) {"${it.id} (${it.score})"}
+        val trainingData = WOMEM_WORLD_CUP_2023_TEAMS.joinToString("\n") { "${it.id} (${it.score})" }
 
-        val authorization = "Bearer $openAiApiKey"
-        val request = ChatCompletionRequest("gpt-3.5-turbo", messages = listOf(
-                Message("system",
-                        """
+        try {
+            val authorization = "Bearer $openAiApiKey"
+            val request = ChatCompletionRequest("gpt-3.5-turbo", messages = listOf(
+                    Message("system",
+                            """
                             Atue como um modelo de analise estatistica para simulação de partidas de futebol feminino
                             Considere os seguintes dados de treinamento,no formato {SIGLA_SELECAO} ({PONTOS_RANKING_FIFA}):
                             $trainingData
                         """.trimIndent()),
-                Message("user", "Simule a partida entre ${team1.id} vs ${team2.id}. Me envie como resposta apenas a sigla da seleção vencedora")
+                    Message("user", "Simule a partida entre ${team1.id} vs ${team2.id}. Me envie como resposta apenas a sigla da seleção vencedora")
 
-        ))
+            ))
 
-        val response = chatGptService.creatChatCompletion(authorization, request)
-        return if (response.choices.first().message.content == team1.id) team1 else team2
+            val response = chatGptService.creatChatCompletion(authorization, request)
+            return if (response.choices.first().message.content == team1.id) team1 else team2
+        } catch (e: NumberFormatException) {
+            println("Possivelmente se chegou aqui é porque acabou a avaliacao da OpenAi")
+            return if (team1.score >= team2.score) team1 else team2
+        }
     }
 
 }
